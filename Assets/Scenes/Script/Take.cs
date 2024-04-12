d1ee48be24d4336604fa562e0d76b6da1df6023b
Léo <lclipet@gaming.tech>
2024-04-12 10:43:57 +0200

    Revert "kja"
    This reverts commit b49ffecd7e02dce1e13dd507a608d98b81f68acd, reversing
    changes made to bf0503ba9ac9571108e24ebd18255f2c66be328b.

--- Assets/Scenes/Script/Take.cs
+++ Assets/Scenes/Script/Take.cs
@@ -5,10 +5,8 @@ public class Take : MonoBehaviour
 {
     // Start is called before the first frame update
     public YeetLight YeetLight;
-    public int ID;
-    public List<GameObject> Inventory;
-    public Transform Hand, 
-        BackPack;
+
+    public Transform Hand;
     public TMP_Text text;
     private bool OnObject;
 
@@ -27,72 +25,21 @@ public class Take : MonoBehaviour
     {
         if (true == OnObject && Input.GetKeyDown(KeyCode.E))
         {
+            YeetLight.On = true;
             GetComponent<Rigidbody>().isKinematic = true;
-            transform.parent = BackPack;
-            transform.position = BackPack.transform.position;
-            //transform.parent = Hand;
-            //transform.position = Hand.transform.position + new Vector3(0,-0.2f,0.3f) /*+ Vector3.RotateTowards(new Vector3(0, 0, 0.3f), new Vector3(0, 0, 3), 1f ,0.0f)*/;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            ID = 1;
-            for (int i = 0; i < BackPack.childCount; i++)
-            {
-                if (BackPack.transform.GetChild(i).gameObject.GetComponent<Items>().ID == ID)
-                {
-                    Inventory[1].transform.parent = BackPack;
-                    Inventory[1].transform.position = BackPack.transform.position + new Vector3(0, -0.2f, 0.3f) /*+ Vector3.RotateTowards(new Vector3(0, 0, 0.3f), new Vector3(0, 0, 3), 1f ,0.0f)*/;
-                    YeetLight.On = true;
-                    GetComponent<Rigidbody>().isKinematic = true;
-                    Inventory[0].transform.parent = Hand;
-                    Inventory[0].transform.position = Hand.transform.position + new Vector3(0, -0.2f, 0.3f) /*+ Vector3.RotateTowards(new Vector3(0, 0, 0.3f), new Vector3(0, 0, 3), 1f ,0.0f)*/;
-                }
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            ID = 2;
-            for (int i = 0; i < BackPack.childCount; i++)
-            {
-                if (BackPack.transform.GetChild(i).gameObject.GetComponent<Items>().ID == ID)
-                {
-                    Inventory[0].transform.parent = BackPack;
-                    Inventory[0].transform.position = BackPack.transform.position + new Vector3(0, -0.2f, 0.3f) /*+ Vector3.RotateTowards(new Vector3(0, 0, 0.3f), new Vector3(0, 0, 3), 1f ,0.0f)*/;
-                    YeetLight.On = true;
-                    GetComponent<Rigidbody>().isKinematic = true;
-                    Inventory[1].transform.parent = Hand;
-                    Inventory[1].transform.position = Hand.transform.position + new Vector3(0, -0.2f, 0.3f) /*+ Vector3.RotateTowards(new Vector3(0, 0, 0.3f), new Vector3(0, 0, 3), 1f ,0.0f)*/;
-                }
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-           for (int i = 0;i < Hand.childCount; i++)
-            {
-                Inventory[i].transform.parent = BackPack;
-                Inventory[i].transform.position = BackPack.transform.position + new Vector3(0, -0.2f, 0.3f) /*+ Vector3.RotateTowards(new Vector3(0, 0, 0.3f), new Vector3(0, 0, 3), 1f ,0.0f)*/;
-            }
-           YeetLight.On = false;
+            transform.parent = Hand;
+            transform.position = Hand.transform.position + new Vector3(0,-0.2f,0.3f) /*+ Vector3.RotateTowards(new Vector3(0, 0, 0.3f), new Vector3(0, 0, 3), 1f ,0.0f)*/;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            text.gameObject.SetActive(true);
-            OnObject = true;
-        }
+        text.gameObject.SetActive(true);
+        OnObject = true;
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            text.gameObject.SetActive(false);
-            OnObject = false;
-        }
+        text.gameObject.SetActive(false);
+        OnObject = false;
     }
 }
